8a498bb7782a64c74c996de806dceb9c9dbf2418
thomasgonda3 <thomasgonda3@gmail.com>
2024-01-08 17:48:11 -0800

    added explicit timeout to fetch requests for mmr

--- mmr.py
+++ mmr.py
@@ -8,28 +8,36 @@ headers = {'Content-type': 'application/json'}
 async def mk8dx_150cc_mmr(url, members):
     base_url = url + '/api/player?'
     players = []
-    async with aiohttp.ClientSession(
-        auth=aiohttp.BasicAuth(
-            "username", "password")) as session:
-        for member in members:
-            request_text = f"discordId={member.id}"
-            request_url = base_url + request_text
-            async with session.get(request_url, headers=headers) as resp:
-                if resp.status != 200:
-                    players.append(None)
-                    continue
-                player_data = await resp.json()
-                if 'mmr' not in player_data.keys():
-                    players.append(Player(member, player_data['name'], None))
-                    continue
-                players.append(
-                    Player(member, player_data['name'], player_data['mmr']))
+    timeout = aiohttp.ClientTimeout(total=10)
+    try:
+        async with aiohttp.ClientSession(
+            timeout=timeout,
+            auth=aiohttp.BasicAuth(
+                "username", "password")) as session:
+            for member in members:
+                request_text = f"discordId={member.id}"
+                request_url = base_url + request_text
+                async with session.get(request_url, headers=headers) as resp:
+                    if resp.status != 200:
+                        players.append(None)
+                        continue
+                    player_data = await resp.json()
+                    if 'mmr' not in player_data.keys():
+                        players.append(
+                            Player(member, player_data['name'], None))
+                        continue
+                    players.append(
+                        Player(member, player_data['name'], player_data['mmr']))
+    except:
+        print(f"Fetch for player {players[0]} has failed.", flush=True)
     return players
 
 
 async def get_mmr_from_discord_id(discord_id):
     base_url = "https://www.mk8dx-lounge.com" + '/api/player?'
+    timeout = aiohttp.ClientTimeout(total=10)
     async with aiohttp.ClientSession(
+        timeout=timeout,
         auth=aiohttp.BasicAuth(
             "username", "password")) as session:
         request_text = f"discordId={discord_id}"
@@ -50,7 +58,8 @@ async def get_mmr(config, members):
 async def mk8dx_150cc_fc(config, name):
     base_url = config["url"] + '/api/player?'
     request_url = base_url + f'name={name}'
-    async with aiohttp.ClientSession() as session:
+    timeout = aiohttp.ClientTimeout(total=15)
+    async with aiohttp.ClientSession(timeout=timeout) as session:
         async with session.get(request_url, headers=headers) as resp:
             if resp.status != 200:
                 return None
