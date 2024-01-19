15c46ec411ff6caaecd8cc2dcd8ea6fb6b6d377a
thomasgonda3 <thomasgonda3@gmail.com>
2024-01-19 08:25:48 -0800

    Changed error message for scoreboard to be more specific

--- cogs/SquadQueue.py
+++ cogs/SquadQueue.py
@@ -387,14 +387,14 @@ class SquadQueue(commands.Cog):
             mogi = discord.utils.find(lambda mogi: mogi.is_room_thread(
                 interaction.channel_id), self.old_events.values())
             if not mogi:
-                await interaction.response.send_message(f"More than {self.MOGI_LIFETIME} minutes have passed since mogi start, the Mogi Object has been deleted.", ephemeral=True)
+                await interaction.response.send_message(f"The Mogi object cannot be found.", ephemeral=True)
                 return
 
         room = discord.utils.find(
             lambda room: room.thread.id == interaction.channel_id, mogi.rooms)
 
         if not room:
-            await interaction.response.send_message(f"More than {self.MOGI_LIFETIME} minutes have passed since mogi start, the Mogi Object has been deleted.", ephemeral=True)
+            await interaction.response.send_message(f"The Thread object cannot be found.", ephemeral=True)
             return
 
         format = round(12/len(room.teams))
